f40571bd1606fda09cf5ad99e5c9947ebb662a56
LogXx <awesome149712@gmail.com>
2024-02-12 14:03:31 +0400

    1 + 2

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,28 @@
-void setup() {
+const int PINS_COUNT = 5;
+const int PINS[PINS_COUNT] = {3, 5, 6, 9, 10};
 
+const int INTER[PINS_COUNT] = {50, 100, 500, 1000, 10000};
+
+unsigned long lastSent[PINS_COUNT];
+int state[PINS_COUNT];
+
+void setup() {
+  unsigned long micros_now = micros();
+  for (int i = 0; i < PINS_COUNT; ++i) {
+    pinMode(PINS[i], OUTPUT);
+    lastSent[i] = micros_now;
+    state[i] = LOW;
+  }
 }
 
 void loop() {
-
+  unsigned long micros_now = micros();
+  for (int i = 0; i < PINS_COUNT; ++i) {
+    int diff = micros_now - lastSent[i];
+    if (diff > INTER[i]) {
+      state[i] = (state[i] == LOW) ? HIGH : LOW;
+      digitalWrite(PINS[i], state[i]);
+      lastSent[i] = micros_now;
+    }
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,33 @@
-void setup() {
+#include <TaskManagerIO.h>
 
+const int PINS_COUNT = 5;
+const int PINS[PINS_COUNT] = {3, 5, 6, 9, 10};
+const TimePeriod INTER[PINS_COUNT] = {repeatMillis(10), repeatMillis(1), repeatMicros(500), repeatMicros(100), repeatMicros(50)};
+
+taskid_t tasks[PINS_COUNT];
+
+class Exec : public Executable {
+private:
+  int _state = LOW;
+
+public:
+  int pin;
+  void exec() override {
+      digitalWrite(pin, _state);
+      _state = (_state == LOW) ? HIGH : LOW;
+  }
+} execs[PINS_COUNT];
+
+void setup() {
+  for (int i = 0; i < PINS_COUNT; ++i) {
+    execs[i] = Exec();
+    execs[i].pin = PINS[i];
+    pinMode(PINS[i], OUTPUT);
+    tasks[i] = taskManager.schedule(INTER[i], &execs[i]);
+  }
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
+
